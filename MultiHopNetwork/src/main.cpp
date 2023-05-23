aefbeb8177307ef7e4f0a795fd0efba06c2533d0
JHirschler <jakob.hirschler@posteo.de>
2023-05-23 08:21:01 +0200

    added basic sending/receiving functionality

--- MultiHopNetwork/src/main.cpp
+++ MultiHopNetwork/src/main.cpp
@@ -1,9 +1,56 @@
-#include <Arduino.h>
+#include <RH_RF95.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SS 18
+#define RST 14
+#define DI0 26
+
+RH_RF95 rf95(SS, DI0);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Initializing...");
+  if (!rf95.init())
+    Serial.println("Init failed");
+  else
+    Serial.println("Init succeeded");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void send()
+{
+  Serial.println("Sending...");
+  uint8_t data[] = "Hello World!";
+  rf95.send(data, sizeof(data));
+  rf95.waitPacketSent();
+  Serial.println("Packet sent!");
+}
+
+void receive()
+{
+  if (rf95.available())
+  {
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      Serial.print("Received: ");
+      Serial.println((char *)buf);
+    }
+    else
+    {
+      Serial.println("Received message is corrupted.");
+    }
+  }
+  else
+  {
+    Serial.println("No package received!");
+  }
+}
+
+void loop()
+{
+  receive();
+
+  // send();
+  delay(1000);
 }
